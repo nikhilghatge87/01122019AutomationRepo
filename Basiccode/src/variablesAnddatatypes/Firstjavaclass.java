2721c9b5b44f152d955bd5c4ef16419c4ea58aaa
nikhilghatge <nikhilvghatge@gmail.com>
2019-12-01 15:25:28 +0530

    First program

---
+++ Basiccode/src/variablesAnddatatypes/Firstjavaclass.java
@@ -0,0 +1,11 @@
+package variablesAnddatatypes;
+
+public class Firstjavaclass {
+
+	public static void main(String[] args) {
+		//TODO Auto-generated method stub
+
+		System.out.println("Hello world");
+	}
+
+}
